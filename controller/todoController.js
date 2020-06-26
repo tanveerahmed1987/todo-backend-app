bfa441ae41da4a13b9f31ac8cce23e7ea6713b76
Tanveer Ahmed <tanveer.ahmed.contractor@move.com>
2020-06-26 23:45:18 +0530

    API separated

--- controller/todoController.js
+++ controller/todoController.js
@@ -6,4 +6,16 @@ module.exports = {
     const todoObj = await Todo.create(todo);
     res.send(todoObj);
   },
+  getTodos: async function (req, res, next) {
+    const todos = await Todo.findAll();
+    res.send(todos);
+  },
+  deleteTodos: async function (req, res, next) {
+    await Todo.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send({});
+  },
 };
--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => res.send("Server is up and running"));
 router.post("/register", userController.createUser);
+
 router.post("/todos", todoController.createTodo);
+router.get("/todos", todoController.getTodos);
+router.delete("/todos/:id", todoController.deleteTodos);
 
 module.exports = router;
